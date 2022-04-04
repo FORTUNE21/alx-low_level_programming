632934dbdf4c8cbf3bef367c1fd2fe1e2d65c742
root <awemary97@gmail.com>
2022-04-04 08:28:21 -0700

    A programs that adds positive numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- *main-print sum of 2 numbers.
+ *main-print multiplies of 2 numbers.
  *@argc:number of commandline arguments.
  *@argv:pointer to an array of commmand line arguments.
  *Return:0-success, non-zero-fail.
